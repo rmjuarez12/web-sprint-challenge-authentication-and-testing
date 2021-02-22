3c748a73a9090dabcd90a34d9ecff53c69c4a9c2
rmjuarez12 <rmjuarez12@gmail.com>
2021-02-21 20:29:42 -0500

    Setup the login endpoint with auth

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,19 +1,23 @@
 //* Setup Router
 const router = require("express").Router();
 
-//* Import bcrypt
+//* Import modules
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+//* Import secrets
+const secrets = require("../config/secrets");
 
 //* Import models
 const authModel = require("./auth-model");
 
 //* Import middleware
-const validateRegister = require("../middleware/validateRegister");
+const validateData = require("../middleware/validateRegister");
 const checkDuplicate = require("../middleware/checkDuplicate");
 
 router.post(
   "/register",
-  [validateRegister, checkDuplicate(authModel)],
+  [validateData, checkDuplicate(authModel)],
   (req, res) => {
     const user = req.body;
 
@@ -28,36 +32,47 @@ router.post(
       })
       .catch((err) => {
         console.log(err);
-        res.status(500).json(err);
+        res.status(500).json({ message: "Error registering user", error: err });
       });
   }
 );
 
-router.post("/login", (req, res) => {
-  res.end("implement login, please!");
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
+router.post("/login", validateData, (req, res) => {
+  const { username, password } = req.body;
 
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
+  authModel
+    .getUserBy("username", username)
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res
+          .status(200)
+          .json({ message: `welcome, ${user.username}`, token: token });
+      } else {
+        res.status(403).json({ message: "invalid credentials" });
       }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Error during login", error: err });
+    });
 });
+
+//* Function to generate token
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: "1h",
+  };
+
+  const secret = secrets.jwtSecret;
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
