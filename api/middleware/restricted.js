0fd51fa122660ad0f6e9107c98c97a07ddc35824
rmjuarez12 <rmjuarez12@gmail.com>
2021-02-21 21:09:13 -0500

    Finished setting up the restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,23 @@
+//* Import jsonwebtokens and the secrets
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers?.authorization?.split(" ")[1];
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Token Invalid", error: err });
+      } else {
+        req.decodedJWT = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "Token required" });
+  }
+
   /*
     IMPLEMENT
 
